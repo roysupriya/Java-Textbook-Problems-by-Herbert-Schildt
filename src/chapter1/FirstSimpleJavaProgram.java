61bf485cd689c2b9a413e932dd6af0c31a80d3c0
Supriya Roy <Supriya.Roy@wdc.com>
2020-03-15 20:06:29 +0530

    Second program

--- src/chapter1/FirstSimpleJavaProgram.java
+++ src/chapter1/FirstSimpleJavaProgram.java
@@ -1,5 +1,7 @@
 package chapter1;
 
 public class FirstSimpleJavaProgram {
-
+	public static void main(String args[]) {
+		System.out.println("Java drives the web.");
+	}
 }
---
+++ src/chapter1/SecondSimpleJavaProgram.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class SecondSimpleJavaProgram {
+	public static void main(String args[]) {
+		int var1, var2;
+		var1 = 1024;
+		
+		System.out.println("var1 contains : " + var1);	
+		
+		var2 = var1/2;
+		
+		System.out.println("var2 contains var1/2 : " + var2);		
+	}
+}
